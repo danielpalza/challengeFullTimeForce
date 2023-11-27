f347f002995c3f63bb962ac5ce3bb73620e67bb9
Daniel Palza <danielpalza@hotmail.com>
2023-11-27 17:43:41 -0300

    added env config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  console.log(`Server running on port: ${configService.get('PORT')}`);
+  await app.listen(configService.get('PORT') as number || 4000);
 }
 bootstrap();
