863f34b571c03f27aa39941f7434b625d2a8719b
Daniel Palza <danielpalza@hotmail.com>
2023-11-27 19:39:35 -0300

    repository resource created

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { RouterModule } from '@nestjs/core';
+import { RepositoryModule } from './repository/repository.module';
 
 @Module({
-  imports: [],
+  imports: [
+    RepositoryModule,
+    ConfigModule.forRoot({  isGlobal: true})],
   controllers: [],
   providers: [],
 })
