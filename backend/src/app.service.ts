17e37dc2dbc9a374720c474d6929ba1c64dad9a1
Daniel Palza <danielpalza@hotmail.com>
2023-11-29 00:56:16 -0300

    transform date formate method created in appService

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
-
+import * as moment from 'moment';
+import 'moment/locale/es';  // Importa el locale español
 @Injectable()
 export class AppService {
- 
+    getRelativeTime(dateString: string): string {
+        moment.locale('es');  // Establece el locale a español
+        const date = moment(dateString);
+        return date.fromNow();
+      }
 }
