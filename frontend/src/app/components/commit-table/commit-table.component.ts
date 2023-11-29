ecbc17c7771e3a6eff84ac701f72fba0e083551d
Daniel Palza <danielpalza@hotmail.com>
2023-11-29 00:58:06 -0300

    added table & service logic for get commits

--- frontend/src/app/components/commit-table/commit-table.component.ts
+++ frontend/src/app/components/commit-table/commit-table.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { RepositoryService } from '../../services/repository.service';
 
 @Component({
   selector: 'app-commit-table',
   templateUrl: './commit-table.component.html',
-  styleUrls: ['./commit-table.component.css']
+  styleUrls: ['./commit-table.component.css'],
 })
 export class CommitTableComponent implements OnInit {
-  commits: any[] = [ ];
-  constructor() { }
+  commits: any = [];
+  branches: string[] = [];
+  branch: string = '';
+  isOpen = false;
+  loading = true;
 
-  ngOnInit(): void {
+  constructor(private repositoryService: RepositoryService) {
+    this.repositoryService.getBranches().subscribe((data: any) => {
+      this.branches = data.branches;
+      if (this.branches.length > 0) {
+        this.branch = this.branches[0];
+        this.getCommits(this.branch);
+      }
+    });
   }
 
+  ngOnInit(): void {}
+
+  getCommits(branch: string) {
+    this.loading = true;
+    this.repositoryService.getCommits(branch).subscribe((data: any) => {
+      this.commits = data.commits;
+      this.branch = data.branch;
+      this.loading = false;
+    });
+  }
+  changeBranch(branch: string) {
+    this.branch = branch;
+    this.isOpen = false;
+    // Aquí puedes añadir más lógica para cambiar la branch
+    this.getCommits(branch);
+  }
 }
